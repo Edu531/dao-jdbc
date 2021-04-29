594d920eacd4e9e7866fb88d19b79f63fcbd72a8
Edu531 <dudu53188@gmail.com>
2021-04-29 15:52:27 -0300

    factory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	
+	
+	
+	
+	
+	
+	@Override
+	public void insert(Seller obj) {
+	
+	}
+
+	@Override
+	public void update(Seller obj) {
+	
+	}
+
+	@Override
+	public void deletById(Seller obj) {
+	
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		return null;
+	}
+
+}
